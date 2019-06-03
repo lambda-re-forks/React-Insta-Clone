3d8986af63be88ede2904b7cbad6b5997c3e5c87
clem9281 <clem9281@gmail.com>
2019-06-03 15:19:09 -0700

    stated scaffolding app and adding scss files

---
+++ instagram-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./SASS/index.scss";
+import dummyData from "./dummy-data";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>InstaClone</h1>
+    </div>
+  );
+}
+
+export default App;
